4481d11b67d005929b1bf9eedd71b10299116719
Shreya Pandey <shreyap2276@gmail.com>
2026-04-14 16:05:21 +0530

    Fixed server.listen and CORS issue

--- back/src/app.js
+++ back/src/app.js
@@ -32,7 +32,7 @@ const app = express();
 
 // 🔹 Middleware
 app.use(cors({
-  origin: "https://foodieapp-1.onrender.com", // frontend URL
+  origin: "http://localhost:5173", // frontend URL
   credentials: true
 }));
 app.use(express.json());
