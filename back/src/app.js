bd9caccd24fca90ca41067efd70fbbfbf9d7e4a0
Shreya Pandey <shreyap2276@gmail.com>
2026-04-14 16:32:58 +0530

    ad

--- back/src/app.js
+++ back/src/app.js
@@ -32,7 +32,7 @@ const app = express();
 
 // 🔹 Middleware
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: "https://foodieappfrontend-omwh.onrender.com", // frontend URL
   credentials: true
 }));
 app.use(express.json());
