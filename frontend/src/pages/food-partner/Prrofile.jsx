d25f0f88cc0ae72cb12a67cc022e7a331dbc2912
Shreya Pandey <shreyap2276@gmail.com>
2026-04-16 16:20:49 +0530

    r

--- frontend/src/pages/food-partner/Prrofile.jsx
+++ frontend/src/pages/food-partner/Prrofile.jsx
@@ -91,7 +91,7 @@ const Prrofile = () => {
 
       <div className="video-section">
         <h3 className="video-title">Videos</h3>
-        <div className="video-grid  flex flex-wrap gap-2">
+        <div className=" flex flex-wrap items-center justify-around gap-2">
           {videos?.map((v, index) => (
             <div key={v.id || index} className="video-wrapper  ">
                
