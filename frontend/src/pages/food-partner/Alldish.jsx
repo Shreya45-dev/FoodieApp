18cd1fca48a2621055f83c8c5c55a9d8ea2e35b3
Shreya Pandey <shreyap2276@gmail.com>
2026-04-18 00:40:02 +0530

    res

--- frontend/src/pages/food-partner/Alldish.jsx
+++ frontend/src/pages/food-partner/Alldish.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
 import { FaPager, FaPaintRoller } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const Alldish = () => {
 
