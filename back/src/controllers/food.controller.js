a987afdf1fb3db3d29399a624c4853d13570d56d
Shreya Pandey <shreyap2276@gmail.com>
2026-04-17 23:29:12 +0530

    res

--- back/src/controllers/food.controller.js
+++ back/src/controllers/food.controller.js
@@ -104,7 +104,7 @@ const deldish=async(req,res)=>{
 }
 const representalldish=async(req,res)=>{
   
-  const dish=await restaurantmodel.find(req.foodpartner._id).populate(foodPartner).sort({createdAt:-1})
+  const dish=await restaurantmodel.findById(req.foodpartner._id).populate('foodPartner').sort({createdAt:-1})
 res.status(200).json({
   message:"dish fetched successfully",
   dish
